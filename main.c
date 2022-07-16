87638971a02045ad8bcb5a53ee6b5ac66963da94
taitai94 <taiapple22@gmail.com>
2022-07-16 08:37:47 -0700

    damn you sdl

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "SDL.h"
+//#include "GL/glew.h"
+int main(int argc, char* argv[])
+{
+
+	//std::cout <<"hello world\n";
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	   SDL_Window* m_sdlWindow = SDL_CreateWindow("name", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 400, SDL_WINDOW_OPENGL);
+	    while(1);
+	    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,2 +1,13 @@
-#inlcude <iostream>
-//add lib
+#include <iostream>
+#include "SDL.h"
+//#include "GL/glew.h"
+int main(int argc, char* argv[])
+{
+
+	//std::cout <<"hello world\n";
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_Window* m_sdlWindow = SDL_CreateWindow("name", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 400, SDL_WINDOW_OPENGL);
+	    while(true);
+	    return 0;
+}
